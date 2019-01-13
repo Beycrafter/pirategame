bdc9812b9f9d06638eec055b2422b3bd81527444
Beycrafter <beycrafter4@gmail.com>
2019-01-12 16:53:17 -0800

    1.0
    basic wind and shooting physics

---
+++ windtest/Assets/sailid.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class sailid : MonoBehaviour {
+
+    public GameObject sail;
+    public GameObject windd;
+
+    public double speed;
+    public double sailrotation;
+    public double locsailrotation;
+    public Quaternion idklol;
+
+    SerializedObject serializedObject;
+
+    // Use this for initialization
+    void Start () {
+      // serializedObject = new UnityEditor.SerializedObject(transform);
+       // SerializedProperty serializedEulerHint = serializedObject.FindProperty("m_LocalEulerAnglesHint");
+        //Debug.Log(serializedEulerHint.vector3Value.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // sailrotation = this.gameObject.transform.eulerAngles.y;
+        sailrotation = WrapAngle(transform.eulerAngles.y);
+        locsailrotation = WrapAngle(transform.localEulerAngles.y);
+
+        Rotate();
+      //  speed = ((sail.transform.rotation.y -windd.transform.rotation.y)/ sail.transform.rotation.y) * windd.GetComponent<wind>().mag;
+      //speed =(1/ Mathf.Abs(sail.transform.rotation.y - windd.transform.rotation.y)) * windd.GetComponent<wind>().mag;
+        speed = (1 / Mathf.Abs(WrapAngle(sail.transform.eulerAngles.y) - WrapAngle(windd.transform.eulerAngles.y))) * windd.GetComponent<wind>().mag; // maybe use dot product
+    }
+    private void Rotate()
+    {
+
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
+        {
+            print("Too many Inputs!");
+
+        }
+        else if (Input.GetKey(KeyCode.U))  //you don't want to be able to rotate both ways if pressing both a and d, however a trumps d, so if you press both down, it will rotate left
+        {
+            // print("Rotating Left");
+            if (locsailrotation > -65) {
+                transform.Rotate(-Vector3.up * 20 * Time.deltaTime);
+            }else
+            {
+
+            }
+        }
+        else if (Input.GetKey(KeyCode.I))
+        {
+            //   print("Rotating Right");
+            if (locsailrotation < 65)
+            {
+                transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+            }else
+            {
+
+            }
+        }
+
+
+    }
+
+    private static float WrapAngle(float angle)
+    {
+        angle %= 360;
+        if (angle > 180)
+            return angle - 360;
+
+        return angle;
+    }
+
+    private static float UnwrapAngle(float angle)
+    {
+        if (angle >= 0)
+            return angle;
+
+        angle = -angle % 360;
+
+        return 360 - angle;
+    }
+
+
+}
---
+++ windtest/Assets/wind.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wind : MonoBehaviour {
+
+    public double mag;
+    public double direction;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        direction = this.gameObject.transform.localEulerAngles.y;
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Too many Inputs!");
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))  //you don't want to be able to rotate both ways if pressing both a and d, however a trumps d, so if you press both down, it will rotate left
+        {
+            // print("Rotating Left");
+           
+            transform.Rotate(-Vector3.up * 20 * Time.deltaTime);
+        
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //   print("Rotating Right");
+            transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+        }
+
+
+    }
+}
